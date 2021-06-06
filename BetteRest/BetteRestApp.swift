a66af59c581499c843275405756151a1418637b5
Aniruddha <aniruddha200baidya@protonmail.com>
2021-06-06 06:19:45 +0100

    Initial Commit

---
+++ BetteRest/BetteRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetteRestApp.swift
+//  BetteRest
+//
+//  Created by administrator on 06/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetteRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetteRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetteRest
+//
+//  Created by administrator on 06/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
