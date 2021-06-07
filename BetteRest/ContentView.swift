844adbdf77548f8f7c410fa982598ab06e0c2723
Aniruddha <aniruddha200baidya@protonmail.com>
2021-06-07 15:36:00 +0100

    Beta 1.0

--- BetteRest/ContentView.swift
+++ BetteRest/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
 	@State private var wakeUpTime = Date()
-	@State private var slpeeHour = 8.00
+	@State private var sleepHour = 8.00
 	@State private var coffeNum = 0
+	
+	@State private var alertTitle = "Bed Time"
+	@State private var alertMessage = ""
+	@State private var showAlert = false
     var body: some View {
 		NavigationView{
 			ZStack{
@@ -21,15 +25,17 @@ struct ContentView: View {
 						
 						DatePicker("", selection: $wakeUpTime, displayedComponents: .hourAndMinute)
 							.labelsHidden()
+							.datePickerStyle(WheelDatePickerStyle())
+							
 					}
 					
 					Spacer()
 					
 					VStack{
-						Text("The number is \(slpeeHour, specifier: "%g")")
+						Text("Sleep Hours \(sleepHour, specifier: "%g")")
 							.font(.headline)
 						
-						Stepper("", value: $slpeeHour, in: 4...12, step: 0.50)
+						Stepper("", value: $sleepHour, in: 4...12, step: 0.50)
 							.labelsHidden()
 					}
 					
@@ -45,11 +51,42 @@ struct ContentView: View {
 					}
 			}
 			.navigationBarTitle("Better Rest", displayMode: .inline)
-			.navigationBarItems(trailing: Button(action: {print("Haha")}, label: {
+			.navigationBarItems(trailing: Button(action: tappedCaculate, label: {
 				Text("Calculate")
-			}))
+			})
+			.alert(isPresented: $showAlert){
+				Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+			}
+			)
 		}
+		
     }
+	func tappedCaculate(){
+		let model = BetterRest()
+		let hourAndMinitue = Calendar.current.dateComponents([.hour, .minute], from: wakeUpTime)
+		let hour = hourAndMinitue.hour ?? 0
+		let minitue = hourAndMinitue.minute ?? 0
+		let time = (hour * 60 * 60) + (minitue * 60)
+		do {
+		
+			let prediction = try model.prediction(wake: Double(time), coffee: Double(coffeNum), actualSleep: sleepHour)
+			let sleepTime = wakeUpTime - prediction.estimatedSleep
+			let formatter = DateFormatter()
+			formatter.timeStyle = .short
+			let bedTime = formatter.string(from: sleepTime)
+			self.alertMessage = "\(bedTime)"
+		}
+		
+		catch{
+			let sleepTime = Date()
+			let formatter = DateFormatter()
+			formatter.dateStyle = .medium
+			let bedTime = formatter.string(from: sleepTime)
+			self.alertMessage = "\(bedTime)"
+		}
+		self.showAlert = true
+		
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
