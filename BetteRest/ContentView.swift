f22ad96baf59965dc2da9419f90127ec6dafb8d3
Aniruddha <aniruddha200baidya@protonmail.com>
2021-06-08 17:19:54 +0100

    Beta 2.0

--- BetteRest/ContentView.swift
+++ BetteRest/ContentView.swift
@@ -12,79 +12,86 @@ struct ContentView: View {
 	@State private var sleepHour = 8.00
 	@State private var coffeNum = 0
 	
-	@State private var alertTitle = "Bed Time"
-	@State private var alertMessage = ""
-	@State private var showAlert = false
+	private var sleepAt: String{
+		self.tappedCaculate()
+	}
+	
     var body: some View {
-		NavigationView{
-			ZStack{
-				Form{
-					VStack{
-						Text("Wake Up Time")
-							.font(.headline)
+		
+			NavigationView{
+				ZStack{
+					
+					Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
+						.frame(minWidth: 300, idealWidth: .infinity, maxWidth: .infinity, minHeight: 900, idealHeight: .infinity, maxHeight: .infinity, alignment: .topLeading)
+
+					
+					VStack(spacing: 10){
 						
-						DatePicker("", selection: $wakeUpTime, displayedComponents: .hourAndMinute)
-							.labelsHidden()
-							.datePickerStyle(WheelDatePickerStyle())
+						Spacer()
+						
+						Section(header: Text("Sleep At:").font(.headline)){
+							Text("\(sleepAt)")
+								.font(.title)
+						}
+						.offset(x:0, y: -60)
+						
+						
+						Section(header: Text("Wake Up Time:").font(.headline)){
 							
-					}
-					
-					Spacer()
-					
-					VStack{
-						Text("Sleep Hours \(sleepHour, specifier: "%g")")
-							.font(.headline)
+							DatePicker("", selection: $wakeUpTime, displayedComponents: .hourAndMinute)
+								.labelsHidden()
+								.datePickerStyle(WheelDatePickerStyle())
+							
+						}
 						
-						Stepper("", value: $sleepHour, in: 4...12, step: 0.50)
-							.labelsHidden()
-					}
-					
-					Spacer()
-					
-					VStack{
-						Text("Coffe Intake \(coffeNum) cups")
-							.font(.headline)
+						Section(header: Text("Sleep Hours: \(sleepHour, specifier: "%g")")
+									.font(.headline)){
+							
+							Stepper("", value: $sleepHour, in: 1...12)
+								.labelsHidden()
+						}
 						
-						Stepper("", value: $coffeNum, in: 0...10)
-							.labelsHidden()
+						Section(header: Text("Coffees Intake: \(coffeNum) cups")
+									.font(.headline)){
+							
+							Stepper("", value: $coffeNum, in: 0...10)
+								.labelsHidden()
+						}
+						
+						Spacer()
+						
 					}
-					}
+				}
+				.navigationBarTitle("Better Rest", displayMode: .inline)
 			}
-			.navigationBarTitle("Better Rest", displayMode: .inline)
-			.navigationBarItems(trailing: Button(action: tappedCaculate, label: {
-				Text("Calculate")
-			})
-			.alert(isPresented: $showAlert){
-				Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-			}
-			)
-		}
 		
     }
-	func tappedCaculate(){
-		let model = BetterRest()
+	func tappedCaculate() -> String{
+		let model = BetterRest1()
 		let hourAndMinitue = Calendar.current.dateComponents([.hour, .minute], from: wakeUpTime)
 		let hour = hourAndMinitue.hour ?? 0
 		let minitue = hourAndMinitue.minute ?? 0
 		let time = (hour * 60 * 60) + (minitue * 60)
+		var result: String
+		var sleepTime: Date
+		var formatter: DateFormatter
+		var prediction: BetterRest1Output
 		do {
 		
-			let prediction = try model.prediction(wake: Double(time), coffee: Double(coffeNum), actualSleep: sleepHour)
-			let sleepTime = wakeUpTime - prediction.estimatedSleep
-			let formatter = DateFormatter()
+			prediction = try model.prediction(wake: Double(time), estimatedSleep: sleepHour, coffee: Double(coffeNum))
+			sleepTime = wakeUpTime - prediction.actualSleep
+			formatter = DateFormatter()
 			formatter.timeStyle = .short
-			let bedTime = formatter.string(from: sleepTime)
-			self.alertMessage = "\(bedTime)"
+			result = formatter.string(from: sleepTime)
 		}
 		
 		catch{
-			let sleepTime = Date()
-			let formatter = DateFormatter()
-			formatter.dateStyle = .medium
-			let bedTime = formatter.string(from: sleepTime)
-			self.alertMessage = "\(bedTime)"
+			result = "Unknown"
 		}
-		self.showAlert = true
+		
+		
+		
+		return result
 		
 	}
 }
